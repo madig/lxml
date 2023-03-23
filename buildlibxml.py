b19f21d480ccaa79bab9193ec52f29ffd3ee2001
Nikolaus Waxweiler <nikolaus.waxweiler@daltonmaag.com>
2023-03-23 18:35:26 +0000

    Try to build universal2 wheels

--- buildlibxml.py
+++ buildlibxml.py
@@ -420,9 +420,17 @@ def cmmi(configure_cmd, build_dir, multicore=None, **call_setup):
 
 def configure_darwin_env(env_setup):
     import platform
-    # configure target architectures on MacOS-X (x86_64 only, by default)
-    major_version, minor_version = tuple(map(int, platform.mac_ver()[0].split('.')[:2]))
-    if major_version > 7:
+    # configure target architectures on macOS; by default compile the
+    # dependencies for the current architecture, but build both when targetting
+    # universal2 wheels.
+    major_version, _ = tuple(map(int, platform.mac_ver()[0].split('.')[:2]))
+    if "BUILD_UNIVERSAL2" in os.environ:
+        env_default = {
+            'CFLAGS': "-arch arm64 -arch x86_64 -O2",
+            'LDFLAGS': "-arch x86_64 -arch arm64",
+            'MACOSX_DEPLOYMENT_TARGET': "11.0"
+        }
+    elif major_version > 7:
         if platform.mac_ver()[2] == "arm64":
             env_default = {
                 'CFLAGS': "-arch arm64 -O2",
@@ -435,8 +443,11 @@ def configure_darwin_env(env_setup):
                 'LDFLAGS': "-arch x86_64",
                 'MACOSX_DEPLOYMENT_TARGET': "10.6"
             }
-        env_default.update(os.environ)
-        env_setup['env'] = env_default
+    else:
+        return
+
+    env_default.update(os.environ)
+    env_setup['env'] = env_default
 
 
 def build_libxml2xslt(download_dir, build_dir,
